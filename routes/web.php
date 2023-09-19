19a4ec6eaf0b592ed8496a3944b6c1c63f61a8aa
itsmee3223 <tamvam.pemberani@gmail.com>
2023-09-19 11:10:37 +0700

    install adminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('/user', HomeController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
