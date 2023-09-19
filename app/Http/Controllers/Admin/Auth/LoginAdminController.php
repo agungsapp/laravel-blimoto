95adb297e557b0cf5351052c48b9969ebb91ee42
itsmee3223 <tamvam.pemberani@gmail.com>
2023-09-19 11:55:12 +0700

    remove adminlte

--- app/Http/Controllers/Admin/Auth/LoginAdminController.php
+++ app/Http/Controllers/Admin/Auth/LoginAdminController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends Controller
+class LoginAdminController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
