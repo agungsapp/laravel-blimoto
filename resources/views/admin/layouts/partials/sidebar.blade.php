5c1716d0355a66f39ce24d4d0b4c30312ed9ccc5
agungsapp <agung.dni19@gmail.com>
2023-09-22 15:44:08 +0700

    update login admin

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -87,6 +87,23 @@
 												</p>
 										</a>
 								</li>
+								<li id="btn-logout" class="nav-item">
+										<a href="#" class="nav-link">
+												<i class="nav-icon fas fa-th"></i>
+												<p>
+														Logout
+														<span class="right badge badge-danger">New</span>
+												</p>
+										</a>
+								</li>
+								<li class="nav-item">
+										<button type="button" data-toggle="modal" data-target="#exampleModal" class="nav-link">
+												<i class="nav-icon fas fa-th"></i>
+												<p>
+														Logout modal
+												</p>
+										</button>
+								</li>
 								<li class="nav-item">
 										<a href="#" class="nav-link">
 												<i class="nav-icon fas fa-th"></i>
@@ -102,3 +119,30 @@
 		</div>
 		<!-- /.sidebar -->
 </aside>
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+		Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+				<div class="modal-content">
+						<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+								</button>
+						</div>
+						<div class="modal-body">
+								...
+						</div>
+						<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								<button type="button" class="btn btn-primary">Save changes</button>
+						</div>
+				</div>
+		</div>
+</div>
