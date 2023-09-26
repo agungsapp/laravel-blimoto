674907179b5b4dc40715085f30c614e3e92cfb70
itsmee3223 <tamvam.pemberani@gmail.com>
2023-09-26 12:48:00 +0700

    gajadai tambah kolom

--- database/migrations/2023_09_19_075000_motor.php
+++ database/migrations/2023_09_19_075000_motor.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('berat');
             $table->string('power');
             $table->integer('harga');
-            $table->integer('min_dp');
             $table->text('deskripsi');
             $table->text('fitur_utama');
             $table->unsignedBigInteger('id_merk');
