88033fa68cd635d9ed01a49e296bf8276a8b82fe
itsmee3223 <tamvam.pemberani@gmail.com>
2023-10-10 15:59:15 +0700

    update controller get detail motor

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ use App\Http\Controllers\User\BrosurController;
 use App\Http\Controllers\User\BrosurKreditController;
 use App\Http\Controllers\User\CicilanMotorController;
 use App\Http\Controllers\User\DealerController;
+use App\Http\Controllers\User\DetailMotorControllerUser;
 use App\Http\Controllers\User\HomeController;
 use App\Http\Controllers\User\InfoLeasingController;
 use App\Http\Controllers\User\MotorTerbaruController;
@@ -89,6 +90,7 @@ Route::get('/get-type', [HomeController::class, 'getTypeMotor']);
 Route::get('/get-merk', [HomeController::class, 'getMerkMotor']);
 Route::get('/get-tenor', [HomeController::class, 'getTenor']);
 Route::get('/get-kota', [HomeController::class, 'getLokasi']);
+Route::get('/detail-motor', [DetailMotorControllerUser::class, 'getDetailMotor']);
 Route::get('/search-motor', [HomeController::class, 'getSearchMotor']);
 // cari dan rekomendasi motor
 Route::get('/cari-cicilan', [CicilanMotorController::class, 'searchAndRecommend']);
