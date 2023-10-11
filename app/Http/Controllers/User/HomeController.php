686f464643bcfd5785f532cfa1f7ed6deed1913e
itsmee3223 <tamvam.pemberani@gmail.com>
2023-10-11 11:56:37 +0700

    fix data structure dp

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -131,7 +131,8 @@ class HomeController extends Controller
             ->where('tenor', $tenor)
             ->pluck('dp');
 
-        $dps = $dps->unique();
+        $dps = $dps->unique()->values()->all();
+        sort($dps);
         return response()->json(['dp' => $dps]);
     }
 
