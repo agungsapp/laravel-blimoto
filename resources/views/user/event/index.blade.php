1e84cc94ddaad137750eaebdedf0bb4a628cd6bb
itsmee3223 <tamvam.pemberani@gmail.com>
2023-11-16 11:00:33 +0700

    halaman event

---
+++ resources/views/user/event/index.blade.php
@@ -0,0 +1,167 @@
+@extends('user.layouts.main')
+@section('content')
+<!-- breadcrumb start -->
+<div class="breadcrumb-main bg-dark">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="breadcrumb-contain">
+          <div>
+            <h2 class="text-white">Event</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- breadcrumb End -->
+
+<section class="section-py-space my-3">
+  <div class="title8 mt-5">
+    <h4>Event BliMoto</h4>
+  </div>
+
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+      <div class="col">
+        <div class="card">
+          <img src="https://scontent.fcgk18-2.fna.fbcdn.net/v/t1.6435-9/188056062_4336456966367080_6545212739371559359_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=810d5f&_nc_eui2=AeGkWAPPkCmG-zJurNlT2Tg-2jYIv7cRPYnaNgi_txE9iRGL3MOagEeUe4bxlGyBL7lrmxkhPiM1N5VdNCnQNsc9&_nc_ohc=OEmLUZdp0l0AX_VTjeF&_nc_ht=scontent.fcgk18-2.fna&oh=00_AfDJS_pnshR7rYhmKNelUFnduX1t6lg-UcShdqlS22WFfw&oe=657D146C" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">Virtual Expo Honda</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Mimin tau kok brosis nungguin banget event ""Virtual Expo Honda Motor"" diadakan lagi kan... Tenang brosis, event ini kembali lagi dan akan akan diadakan pada tanggal 25-31 Mei 2021... Udah saatnya brosis miliki motor Honda impian brosis dengan banyak promo yang super heboh 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              25-31 Mei 2021
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://honda-hayati.com/wp-content/uploads/2018/09/Event-Honda-Premium-Matic-Day.jpg" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">Event Honda Premium Matic Day</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Honda Premium Matic Day kini datang lagi. Event khusus yang menghadirkan jajaran line up Motor matic premium Honda, dikemas dengan serangkaian kegiatan seru 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              15-16 September 2018
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://www.yamaha-motor.co.id/uploads/content/2022060611015089861W84053.jpg" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">FAZZIO FESTIVAL</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Produk terbaru dari kategori Classy Yamaha terus menuai perhatian, dengan keunggulan desain, teknologi, serta fitur-fitur premium menjadi daya tarik Yamaha Fazzio Hybrid-Connected. Desain simple dan berkelas membuatnya seperti white canvas yang mudah disesuaikan oleh beragam karakter dan gaya konsumennya. Menanggapi respon positif sekaligus mewadahi kreativitas para konsumen. Dan kota Semarang akan menjadi pembuka hadirnya Fazzio Festival di Mal Ciputra Semarang 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              12 – 18 Mei 2022
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://honda-hayati.com/wp-content/uploads/2018/09/Event-Honda-Premium-Matic-Day.jpg" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">Event Honda Premium Matic Day</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Honda Premium Matic Day kini datang lagi. Event khusus yang menghadirkan jajaran line up Motor matic premium Honda, dikemas dengan serangkaian kegiatan seru 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              15-16 September 2018
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://www.yamaha-motor.co.id/uploads/content/2022060611015089861W84053.jpg" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">FAZZIO FESTIVAL</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Produk terbaru dari kategori Classy Yamaha terus menuai perhatian, dengan keunggulan desain, teknologi, serta fitur-fitur premium menjadi daya tarik Yamaha Fazzio Hybrid-Connected. Desain simple dan berkelas membuatnya seperti white canvas yang mudah disesuaikan oleh beragam karakter dan gaya konsumennya. Menanggapi respon positif sekaligus mewadahi kreativitas para konsumen. Dan kota Semarang akan menjadi pembuka hadirnya Fazzio Festival di Mal Ciputra Semarang 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              12 – 18 Mei 2022
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img src="https://scontent.fcgk18-2.fna.fbcdn.net/v/t1.6435-9/188056062_4336456966367080_6545212739371559359_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=810d5f&_nc_eui2=AeGkWAPPkCmG-zJurNlT2Tg-2jYIv7cRPYnaNgi_txE9iRGL3MOagEeUe4bxlGyBL7lrmxkhPiM1N5VdNCnQNsc9&_nc_ohc=OEmLUZdp0l0AX_VTjeF&_nc_ht=scontent.fcgk18-2.fna&oh=00_AfDJS_pnshR7rYhmKNelUFnduX1t6lg-UcShdqlS22WFfw&oe=657D146C" alt="..." />
+          <div class="card-body">
+            <div class="row">
+              <div class="col">
+                <h5 class="card-title">Virtual Expo Honda</h5>
+              </div>
+              <div class="col-2">
+                <a href="https://bit.ly/WahanaHondaVirtualExpo">
+                  <i class="fa fa-location-arrow fs-5" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+            <p class="card-text">
+              Mimin tau kok brosis nungguin banget event ""Virtual Expo Honda Motor"" diadakan lagi kan... Tenang brosis, event ini kembali lagi dan akan akan diadakan pada tanggal 25-31 Mei 2021... Udah saatnya brosis miliki motor Honda impian brosis dengan banyak promo yang super heboh 🎉
+            </p>
+            <p class="card-text mt-2 text-success fw-bold">
+              25-31 Mei 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
