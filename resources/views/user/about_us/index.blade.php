c67bdadd0f81105c4a9b49096db4d2a68f876c63
agungsapp <agung.dni19@gmail.com>
2023-12-02 19:18:56 +0700

    update about us

--- resources/views/user/about_us/index.blade.php
+++ resources/views/user/about_us/index.blade.php
@@ -1,187 +1,96 @@
 @extends('user.layouts.main') @section('content')
-<!-- breadcrumb start -->
-<div class="breadcrumb-main bg-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="breadcrumb-contain">
-                    <div>
-                        <h2 class="text-white">About Us</h2>
-                        <ul>
-                            <li>
-                                <a class="text-white" href="javascript:void(0)">Home</a>
-                            </li>
-                            <li><i class="text-white fa fa-angle-double-right"></i></li>
-                            <li>
-                                <a class="text-white" href="javascript:void(0)">About Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- breadcrumb End -->
+		<!-- breadcrumb start -->
+		<div class="breadcrumb-main bg-dark">
+				<div class="container">
+						<div class="row">
+								<div class="col">
+										<div class="breadcrumb-contain">
+												<div>
+														<h2 class="text-white">About Us</h2>
+														<ul>
+																<li>
+																		<a class="text-white" href="javascript:void(0)">Home</a>
+																</li>
+																<li><i class="fa fa-angle-double-right text-white"></i></li>
+																<li>
+																		<a class="text-white" href="javascript:void(0)">About Us</a>
+																</li>
+														</ul>
+												</div>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+		<!-- breadcrumb End -->
 
-<section>
-    <div class="bg-light">
-        <div class="container py-5">
-            <div class="row h-100 align-items-center py-5">
-                <div class="col-lg-6">
-                    <h1 class="display-4">About us page</h1>
-                    <p class="lead text-muted mb-0">
-                        Create a minimal about us page using Bootstrap 4.
-                    </p>
-                </div>
-                <div class="col-lg-6 d-none d-lg-block">
-                    <img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid" />
-                </div>
-            </div>
-        </div>
-    </div>
+		<section id="hero">
+				<div style="height: 600px;" class="bg-primary"></div>
+		</section>
 
-    <div class="bg-white py-5">
-        <div class="container py-5">
-            <div class="row align-items-center mb-5">
-                <div class="col-lg-6 order-2 order-lg-1">
-                    <i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-                    <h2 class="font-weight-light">Lorem ipsum dolor sit amet</h2>
-                    <p class="font-italic text-muted mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                </div>
-                <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
-                    <img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0" />
-                </div>
-            </div>
-            <div class="row align-items-center">
-                <div class="col-lg-5 px-5 mx-auto">
-                    <img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0" />
-                </div>
-                <div class="col-lg-6">
-                    <i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
-                    <h2 class="font-weight-light">Lorem ipsum dolor sit amet</h2>
-                    <p class="font-italic text-muted mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+		<section class="bg-white" style="height: 2000px;">
+				<div class="container-fluid px-5">
+						<div class="row">
+								<div class="col-6">
+										<div class="welcome-item">
+												<div class="welcome-wrapper p-5">
+														<h2>BLIMOTO</h2>
+														<h3>Selamat datang di Blimoto !</h3>
+														<p>
+																Sebuah platform yang mempermudah anda dalam mencari informasi terkait harga, angsuran hingga diskon yang
+																sesungguhnya dari sebuah produk kendaraan yang sedang anda cari. Blimoto menyediakan fitur yang membuat
+																anda
+																cukup diam ditempat tanpa harus mengunjungi dealer konfensional hanya untuk mencari informasi terkait,
+																Blimoto
+																juga memberikan pelayanan untuk anda agar dapat melakukan pembelian kendaraan tanpa harus beranjak dari
+																tempat
+																anda berada.
 
-    <div class="bg-light py-5">
-        <div class="container py-5">
-            <div class="row mb-4">
-                <div class="col-lg-5">
-                    <h2 class="display-4 font-weight-light">Founder</h2>
-                    <p class="font-italic text-muted">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    </p>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <!-- Team item-->
-                <div class="col-xl-3 col-sm-6 mb-5">
-                    <div class="bg-white rounded shadow-sm py-5 px-4">
-                        <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-4.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                        <h5 class="mb-0">Manuella Nevoresky</h5>
-                        <span class="small text-uppercase text-muted">CEO - Founder</span>
-                        <ul class="social mb-0 list-inline mt-3">
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End-->
-
-                <!-- Team item-->
-                <div class="col-xl-3 col-sm-6 mb-5">
-                    <div class="bg-white rounded shadow-sm py-5 px-4">
-                        <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                        <h5 class="mb-0">Samuel Hardy</h5>
-                        <span class="small text-uppercase text-muted">CEO - Founder</span>
-                        <ul class="social mb-0 list-inline mt-3">
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End-->
-
-                <!-- Team item-->
-                <div class="col-xl-3 col-sm-6 mb-5">
-                    <div class="bg-white rounded shadow-sm py-5 px-4">
-                        <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                        <h5 class="mb-0">Tom Sunderland</h5>
-                        <span class="small text-uppercase text-muted">CEO - Founder</span>
-                        <ul class="social mb-0 list-inline mt-3">
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End-->
-
-                <!-- Team item-->
-                <div class="col-xl-3 col-sm-6 mb-5">
-                    <div class="bg-white rounded shadow-sm py-5 px-4">
-                        <img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                        <h5 class="mb-0">John Tarly</h5>
-                        <span class="small text-uppercase text-muted">CEO - Founder</span>
-                        <ul class="social mb-0 list-inline mt-3">
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-instagram"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="social-link"><i class="fa fa-linkedin"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End-->
-            </div>
-        </div>
-    </div>
-</section>
-
+																karena BLIMOTO solusi kita bersma.
+														</p>
+												</div>
+										</div>
+								</div>
+								<div class="col-6">
+										<div class="fitur-kami-wrapper p-5">
+												<h4 class="mb-3 text-black">Jelajahi Fitur Kami : </h4>
+												<div class="row">
+														<div class="col-6 p-2">
+																<div class="rounded-lg p-4" style="border: 1px solid #D22232; background-color: #FFF6F7;">
+																		<h5 class="text-black">Up to Date !</h5>
+																		<p class="text-black">Blimoto selalu memberikan Update
+																				motor terbaru yang kamu cari!</p>
+																		<a href="#" class="btn btn-basic btn-fitur-kami">Lihat Fitur</a>
+																</div>
+														</div>
+														<div class="col-6 p-2">
+																<div class="rounded-lg p-4" style="border: 1px solid #D22232; background-color: #FFF6F7;">
+																		<h5 class="text-black">Up to Date !</h5>
+																		<p class="text-black">Blimoto selalu memberikan Update
+																				motor terbaru yang kamu cari!</p>
+																		<a href="#" class="btn btn-basic btn-fitur-kami">Lihat Fitur</a>
+																</div>
+														</div>
+														<div class="col-6 p-2">
+																<div class="rounded-lg p-4" style="border: 1px solid #D22232; background-color: #FFF6F7;">
+																		<h5 class="text-black">Up to Date !</h5>
+																		<p class="text-black">Blimoto selalu memberikan Update
+																				motor terbaru yang kamu cari!</p>
+																		<a href="#" class="btn btn-basic btn-fitur-kami">Lihat Fitur</a>
+																</div>
+														</div>
+														<div class="col-6 p-2">
+																<div class="rounded-lg p-4" style="border: 1px solid #D22232; background-color: #FFF6F7;">
+																		<h5 class="text-black">Up to Date !</h5>
+																		<p class="text-black">Blimoto selalu memberikan Update
+																				motor terbaru yang kamu cari!</p>
+																		<a href="#" class="btn btn-basic btn-fitur-kami">Lihat Fitur</a>
+																</div>
+														</div>
+												</div>
+										</div>
+								</div>
+						</div>
+				</div>
+		</section>
 @endsection
