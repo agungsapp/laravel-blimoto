c9ed3acf5f3603e15d3e18a732389811bf64c2b8
itsmee3223 <tamvam.pemberani@gmail.com>
2023-12-02 14:55:11 +0700

    login user

--- app/Http/Controllers/User/UserLoginController.php
+++ app/Http/Controllers/User/UserLoginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class UserLoginController extends Controller
 {
@@ -15,6 +18,9 @@ class UserLoginController extends Controller
      */
     public function index()
     {
+        if (Auth::check()) {
+            return redirect()->route('dashboard');
+        }
         return view('user.auth.login');
     }
 
@@ -82,8 +88,86 @@ class UserLoginController extends Controller
      */
     public function destroy()
     {
-        //
-        Auth::logout(); // Lakukan proses logout
-        return redirect()->route('login'); // Redirect ke halaman login atau halaman lain yang diinginkan
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function requestOTP(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nohp' => ['required', 'numeric'],
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $nomor = $request->input('nohp');
+        $otp = $this->generateOTP();
+
+        // Save OTP to user record in the database
+        $user = User::where('nomor_hp', $nomor)->first();
+        if (!$user) {
+            return redirect()->back();
+        }
+        $user->update([
+            'kode_otp' => $otp,
+            'otp_expire' => now()->addMinutes(5),
+        ]);
+
+        // Send OTP to the user (using Twilio as an example)
+        // $this->sendOTP($nomor, $otp);
+        Session::put('verified_nomor', $nomor);
+        return view('user.auth.verifikasi_login', [
+            'otp' => $otp,
+            'nomor' => $nomor
+        ]);
+    }
+
+    public function verifyOTP(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'otp_user' => ['required', 'numeric'],
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $nomor = Session::get('verified_nomor');
+        $otp = $request->input('otp_user');
+
+        // Retrieve the user by phone number and OTP
+        $user = User::where('nomor_hp', $nomor)
+            ->where('kode_otp', $otp)
+            ->where('otp_expire', '>', now())
+            ->first();
+
+        if (!$user) {
+            // Invalid OTP or expired
+            return redirect()->back()->withErrors(['otp_user' => 'Invalid OTP or expired.'])->withInput();
+        }
+
+        // Log in the user
+        Auth::login($user);
+
+        // Clear the OTP-related fields in the user record
+        $user->update([
+            'kode_otp' => null,
+            'otp_expire' => null,
+        ]);
+
+        // Redirect to the dashboard or any desired page after successful login
+        return redirect()->route('/');
+    }
+
+    private function generateOTP($length = 4)
+    {
+        $digits = '0123456789';
+        $otp = '';
+        for ($i = 0; $i < $length; $i++) {
+            $otp .= $digits[rand(0, strlen($digits) - 1)];
+        }
+        return $otp;
     }
 }
