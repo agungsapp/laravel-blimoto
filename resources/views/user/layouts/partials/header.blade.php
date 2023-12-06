ec4a77f3f76d128e954c1eeb4ffb6c9498c1de00
agungsapp <agung.dni19@gmail.com>
2023-12-06 18:09:47 +0700

    fixing feature search

--- resources/views/user/layouts/partials/header.blade.php
+++ resources/views/user/layouts/partials/header.blade.php
@@ -63,8 +63,8 @@
 																		</a>
 																		<ul id="select-lokasi-user" class="select-lokasi-user">
 																				<li><a href="#" data-id="1">Jakarta Selatan</a></li>
-																				<li><a href="#" data-id="2">Depok</a></li>
-																				<li><a href="#" data-id="3">Bogor</a></li>
+																				<li><a href="#" data-id="2">Bogor</a></li>
+																				<li><a href="#" data-id="3">Depok</a></li>
 																				<li><a href="#" data-id="4">Tanggerang</a></li>
 																				<li><a href="#" data-id="5">Bekasi</a></li>
 																		</ul>
