58799216f076db3efbaea776f0dd912b9f569cf3
itsmee3223 <tamvam.pemberani@gmail.com>
2023-12-14 16:51:09 +0700

    input password

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -27,7 +27,7 @@
               </div>
               <div class="form-group col-md-6">
                 <label for="input-password">Password</label>
-                <input name="password" type="text" class="form-control" placeholder="Masukan password">
+                <input name="password" type="password" class="form-control" placeholder="Masukan password">
               </div>
             </div>
           </div>
