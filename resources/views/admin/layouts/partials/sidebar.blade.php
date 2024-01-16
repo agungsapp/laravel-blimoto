53f13e9c113d66e359c3cd576d3a667aa8841464
itsmee3223 <tamvam.pemberani@gmail.com>
2024-01-16 09:45:48 +0700

    sales setting menu

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -22,9 +22,8 @@
 
 		<!-- Sidebar Menu -->
 		<nav class="mt-2">
+			@if (Auth::guard('admin')->check())
 			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-				<!-- Add icons to the links using the .nav-icon class
-															with font-awesome or any other icon font library -->
 
 				<li class="nav-item">
 					<a href="{{ route('admin.dashboard.index') }}" class="nav-link {{ \Route::is('admin.dashboard.*') ? 'active' : '' }}">
@@ -36,7 +35,6 @@
 					</a>
 				</li>
 
-				@if (Auth::guard('admin')->check())
 				<li class="nav-item">
 					<a href="{{ route('admin.users.index') }}" class="nav-link {{ \Route::is('admin.users.*') ? 'active' : '' }}">
 						<i class="nav-icon fa fa-user"></i>
@@ -45,7 +43,6 @@
 						</p>
 					</a>
 				</li>
-				@endif
 
 				<li class="nav-item">
 					<a href="{{ route('admin.company-profile.index') }}" class="nav-link {{ \Route::is('admin.company-profile.*') ? 'active' : '' }}">
@@ -116,6 +113,7 @@
 						</li>
 					</ul>
 				</li>
+
 				{{-- master kota --}}
 				<li class="nav-item {{ \Route::is('admin.kota.*') || \Route::is('admin.kota-motor.*') ? 'menu-open' : '' }}">
 					<a href="#" class="nav-link {{ \Route::is('admin.kota.*') || \Route::is('admin.kota-motor.*') ? 'active' : '' }}">
@@ -140,6 +138,7 @@
 						</li>
 					</ul>
 				</li>
+
 				{{-- master sales --}}
 				<li class="nav-item {{ \Route::is('admin.sales.*') || \Route::is('admin.penjualan.data.*') || \Route::is('admin.penjualan.hasil.*') || \Route::is('admin.penjualan.spk.*') || \Route::is('admin.pembayaran.*') ? 'menu-open' : '' }}">
 					<a href="#" class="nav-link {{ \Route::is('admin.sales.*') || \Route::is('admin.penjualan.data.*') || \Route::is('admin.penjualan.hasil.*') || \Route::is('admin.penjualan.spk.*') || \Route::is('admin.pembayaran.*')  ? 'active' : '' }}">
@@ -149,16 +148,14 @@
 							<i class="right fas fa-angle-left"></i>
 						</p>
 					</a>
-					@if (Auth::guard('admin')->check())
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
 							<a href="{{ route('admin.sales.index') }}" class="nav-link {{ \Route::is('admin.sales.*') ? 'active' : '' }}">
 								<i class="far fa-circle nav-icon"></i>
-								<p>Tambah Akun Sales</p>
+								<p>Akun Sales</p>
 							</a>
 						</li>
 					</ul>
-					@endif
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
 							<a href="{{ route('admin.penjualan.data.index') }}" class="nav-link {{ \Route::is('admin.penjualan.data.*') ? 'active' : '' }}">
@@ -172,189 +169,201 @@
 								<p>Master Hasil</p>
 							</a>
 						</li>
-						{{-- <li class="nav-item">
-							<a href="{{ route('admin.pembayaran.index') }}" class="nav-link {{ \Route::is('admin.pembayaran.*') ? 'active' : '' }}">
-						<i class="far fa-circle nav-icon"></i>
-						<p>Pembayaran</p>
-						</a>
-				</li> --}}
-				<!-- <li class="nav-item">
-							<a href="{{ route('admin.penjualan.spk.index') }}" class="nav-link {{ \Route::is('admin.penjualan.spk.*') ? 'active' : '' }}">
+					</ul>
+				</li>
+
+				{{-- master pembayaran --}}
+				<li class="nav-item {{ \Route::is('admin.sudah-bayar') || \Route::is('admin.belum-bayar') ? 'menu-open' : '' }}">
+					<a href="#" class="nav-link {{ \Route::is('admin.sudah-bayar') || \Route::is('admin.belum-bayar') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-city"></i>
+						<p>
+							Data Pembayaran
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="{{ route('admin.sudah-bayar') }}" class="nav-link {{ \Route::is('admin.sudah-bayar') ? 'active' : '' }}">
 								<i class="far fa-circle nav-icon"></i>
-								<p>SPK</p>
+								<p>Sudah Bayar</p>
 							</a>
-						</li> -->
-			</ul>
-			</li>
+						</li>
+						<li class="nav-item">
+							<a href="{{ route('admin.belum-bayar') }}" class="nav-link {{ \Route::is('admin.belum-bayar') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Belum Bayar</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-			{{-- master pembayaran --}}
-			<li class="nav-item {{ \Route::is('admin.sudah-bayar') || \Route::is('admin.belum-bayar') ? 'menu-open' : '' }}">
-				<a href="#" class="nav-link {{ \Route::is('admin.sudah-bayar') || \Route::is('admin.belum-bayar') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-city"></i>
-					<p>
-						Data Pembayaran
-						<i class="right fas fa-angle-left"></i>
-					</p>
-				</a>
-				<ul class="nav nav-treeview">
-					<li class="nav-item">
-						<a href="{{ route('admin.sudah-bayar') }}" class="nav-link {{ \Route::is('admin.sudah-bayar') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Sudah Bayar</p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="{{ route('admin.belum-bayar') }}" class="nav-link {{ \Route::is('admin.belum-bayar') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Belum Bayar</p>
-						</a>
-					</li>
-				</ul>
-			</li>
+				{{-- master price list --}}
+				<li class="nav-item {{ \Route::is('admin.cicilan.*') ? 'menu-open' : '' }}">
+					<a href="#" class="nav-link {{ \Route::is('admin.cicilan.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-money-bill" aria-hidden="true"></i>
+						<p>
+							Master Price List
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="{{ route('admin.cicilan.create') }}" class="nav-link {{ \Route::is('admin.cicilan.create') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Tambah Price List</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="{{ route('admin.cicilan.index') }}" class="nav-link {{ \Route::is('admin.cicilan.index') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Data Price List</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-			{{-- master price list --}}
-			<li class="nav-item {{ \Route::is('admin.cicilan.*') ? 'menu-open' : '' }}">
-				<a href="#" class="nav-link {{ \Route::is('admin.cicilan.*') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-money-bill" aria-hidden="true"></i>
-					<p>
-						Master Price List
-						<i class="right fas fa-angle-left"></i>
-					</p>
-				</a>
-				<ul class="nav nav-treeview">
-					<li class="nav-item">
-						<a href="{{ route('admin.cicilan.create') }}" class="nav-link {{ \Route::is('admin.cicilan.create') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Tambah Price List</p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="{{ route('admin.cicilan.index') }}" class="nav-link {{ \Route::is('admin.cicilan.index') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Data Price List</p>
-						</a>
-					</li>
-				</ul>
-			</li>
+				{{-- master slik --}}
+				<li class="nav-item {{ \Route::is('admin.slik-bi.*') || \Route::is('admin.type-slik-bi.*') || \Route::is('admin.status-bi.*') ? 'menu-open' : '' }}">
+					<a href="#" class="nav-link {{ \Route::is('admin.slik-bi.*') || \Route::is('admin.type-slik-bi.*') || \Route::is('admin.status-bi.*') ? 'active' : '' }}">
+						<i class="nav-icon fa fa-id-card" aria-hidden="true"></i>
+						<p>
+							Master Slik
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="{{ route('admin.slik-bi.index') }}" class="nav-link {{ \Route::is('admin.slik-bi.*') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Data Cek Slik BI</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="{{ route('admin.type-slik-bi.index') }}" class="nav-link {{ \Route::is('admin.type-slik-bi.*') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Data Type Slik BI</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="{{ route('admin.status-bi.index') }}" class="nav-link {{ \Route::is('admin.status-bi.*') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Status BI</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-			{{-- master slik --}}
-			<li class="nav-item {{ \Route::is('admin.slik-bi.*') || \Route::is('admin.type-slik-bi.*') || \Route::is('admin.status-bi.*') ? 'menu-open' : '' }}">
-				<a href="#" class="nav-link {{ \Route::is('admin.slik-bi.*') || \Route::is('admin.type-slik-bi.*') || \Route::is('admin.status-bi.*') ? 'active' : '' }}">
-					<i class="nav-icon fa fa-id-card" aria-hidden="true"></i>
-					<p>
-						Master Slik
-						<i class="right fas fa-angle-left"></i>
-					</p>
-				</a>
-				<ul class="nav nav-treeview">
-					<li class="nav-item">
-						<a href="{{ route('admin.slik-bi.index') }}" class="nav-link {{ \Route::is('admin.slik-bi.*') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Data Cek Slik BI</p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="{{ route('admin.type-slik-bi.index') }}" class="nav-link {{ \Route::is('admin.type-slik-bi.*') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Data Type Slik BI</p>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="{{ route('admin.status-bi.index') }}" class="nav-link {{ \Route::is('admin.status-bi.*') ? 'active' : '' }}">
-							<i class="far fa-circle nav-icon"></i>
-							<p>Status BI</p>
-						</a>
-					</li>
-				</ul>
-			</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.blog.index') }}" class="nav-link {{ \Route::is('admin.blog.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-newspaper"></i>
+						<p>
+							Data Postingan
+						</p>
+					</a>
+				</li>
 
-			<li class="nav-item">
-				<a href="{{ route('admin.blog.index') }}" class="nav-link {{ \Route::is('admin.blog.*') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-newspaper"></i>
-					<p>
-						Data Postingan
-					</p>
-				</a>
-			</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.hook.index') }}" class="nav-link {{ \Route::is('admin.hook.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-images"></i>
+						<p>
+							Data Hook
+						</p>
+					</a>
+				</li>
 
-			<li class="nav-item">
-				<a href="{{ route('admin.hook.index') }}" class="nav-link {{ \Route::is('admin.hook.*') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-images"></i>
-					<p>
-						Data Hook
-					</p>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="{{ route('admin.leasing-motor.index') }}" class="nav-link {{ \Route::is('admin.leasing-motor.*') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-building"></i>
-					<p>
-						Data Leasing
-					</p>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="{{ route('admin.dealer-motor.index') }}" class="nav-link {{ \Route::is('admin.dealer-motor.*') ? 'active' : '' }}">
-					<i class="nav-icon fa fa-industry" aria-hidden="true"></i>
-					<p>
-						Data Dealer
-					</p>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="{{ route('admin.mitra.index') }}" class="nav-link {{ \Route::is('admin.mitra.*') ? 'active' : '' }}">
-					<i class="nav-icon fas fa-users"></i>
-					<p>
-						Data Mitra
-					</p>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="{{ route('admin.event.index') }}" class="nav-link {{ \Route::is('admin.event.*') ? 'active' : '' }}">
-					<i class="nav-icon fa fa-bullseye"></i>
-					<p>
-						Data Event
-					</p>
-				</a>
-			</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.leasing-motor.index') }}" class="nav-link {{ \Route::is('admin.leasing-motor.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-building"></i>
+						<p>
+							Data Leasing
+						</p>
+					</a>
+				</li>
 
-			<li class="nav-item">
-				<a href="{{ route('admin.promo.index') }}" class="nav-link {{ \Route::is('admin.promo.*') ? 'active' : '' }}">
-					<i class="nav-icon fa fa-bell"></i>
-					<p>
-						Data Promo
-					</p>
-				</a>
-			</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.dealer-motor.index') }}" class="nav-link {{ \Route::is('admin.dealer-motor.*') ? 'active' : '' }}">
+						<i class="nav-icon fa fa-industry" aria-hidden="true"></i>
+						<p>
+							Data Dealer
+						</p>
+					</a>
+				</li>
 
-			{{-- <li class="nav-item">
+				<li class="nav-item">
+					<a href="{{ route('admin.mitra.index') }}" class="nav-link {{ \Route::is('admin.mitra.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-users"></i>
+						<p>
+							Data Mitra
+						</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="{{ route('admin.event.index') }}" class="nav-link {{ \Route::is('admin.event.*') ? 'active' : '' }}">
+						<i class="nav-icon fa fa-bullseye"></i>
+						<p>
+							Data Event
+						</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
 					<a href="{{ route('admin.promo.index') }}" class="nav-link {{ \Route::is('admin.promo.*') ? 'active' : '' }}">
-			<i class="nav-icon fa fa-bell"></i>
-			<p>
-				Data Promo
-			</p>
-			</a>
-			</li> --}}
-			<li class="nav-item">
-				<a href="{{ route('admin.logout') }}" class="nav-link">
-					<i class="nav-icon fas fa-sign-out-alt"></i>
-					<p>
-						Logout
-					</p>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="nav-icon fas fa-th"></i>
-					<p>
-						Sample
-						<span class="right badge badge-danger">New</span>
-					</p>
-				</a>
-			</li>
+						<i class="nav-icon fa fa-bell"></i>
+						<p>
+							Data Promo
+						</p>
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a href="{{ route('admin.logout') }}" class="nav-link">
+						<i class="nav-icon fas fa-sign-out-alt"></i>
+						<p>
+							Logout
+						</p>
+					</a>
+				</li>
+
 			</ul>
+			@else
+			{{-- master sales --}}
+			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+				<li class="nav-item {{ \Route::is('admin.sales.*') || \Route::is('admin.penjualan.data.*') ? 'menu-open' : '' }}">
+					<a href="#" class="nav-link {{ \Route::is('admin.sales.*') || \Route::is('admin.penjualan.data.*')  ? 'active' : '' }}">
+						<i class="nav-icon fas fa-city"></i>
+						<p>
+							Master Sales
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="{{ route('admin.penjualan.data.index') }}" class="nav-link {{ \Route::is('admin.penjualan.data.*') ? 'active' : '' }}">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Data Penjualan</p>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.sales-settings.index') }}" class="nav-link {{ \Route::is('admin.sales-settings.*') ? 'active' : '' }}">
+						<i class="nav-icon fas fa-users"></i>
+						<p>
+							Setting Akun
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="{{ route('admin.logout') }}" class="nav-link">
+						<i class="nav-icon fas fa-sign-out-alt"></i>
+						<p>
+							Logout
+						</p>
+					</a>
+				</li>
+			</ul>
+			@endif
 		</nav>
-		<!-- /.sidebar-menu -->
 	</div>
-	<!-- /.sidebar -->
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\Admin\AdminPembayaranController;
 use App\Http\Controllers\Admin\AdminPenjualanController;
 use App\Http\Controllers\Admin\AdminPromoController;
 use App\Http\Controllers\Admin\AdminSalesController;
+use App\Http\Controllers\Admin\AdminSalesSettingController;
 use App\Http\Controllers\Admin\AdminSlikController;
 use App\Http\Controllers\Admin\AdminSPKController;
 use App\Http\Controllers\Admin\AdminStatusBiController;
@@ -153,56 +154,64 @@ Route::prefix('app')->name('admin.')->group(function () {
     Route::get('login', [LoginAdminController::class, 'index']);
     Route::post('login', [LoginAdminController::class, 'procesLogin'])->name('login');
     Route::get('logout', [LogoutAdminController::class, 'logout'])->name('logout');
-
+    
     Route::middleware(['auth.admin.sales'])->group(function () {
         Route::resource('dashboard', DashboardController::class);
-        Route::resource('motor', MotorController::class);
-        Route::resource('type-motor', TypeMotorController::class);
-        Route::resource('merk-motor', MerkMotorController::class);
-        Route::resource('best-motor', BestMotorController::class);
-        Route::resource('leasing-motor', LeasingMotorController::class);
-        Route::resource('kota', KotaController::class);
-        Route::resource('hook', HookController::class);
-        Route::resource('detail-motor', DetailMotorController::class);
-        Route::resource('brosur-motor', BrosurMotorController::class);
-        Route::resource('kota-motor', MotorKotaController::class);
-        Route::resource('blog', BlogController::class);
-        Route::resource('cicilan', AdminCicilanMotorController::class);
-        Route::resource('dealer-motor', AdminDealerController::class);
-        Route::resource('mitra', MitraKamiController::class);
-        Route::resource('diskon-motor', AdminDiskonMotorController::class);
-        Route::resource('mtr-best-motor', AdminMtrBestMotorController::class);
-        Route::resource('event', AdminEventController::class);
-        Route::resource('company-profile', AdminCompanyProfileController::class);
-        Route::resource('slik-bi', AdminSlikController::class);
-        Route::resource('status-bi', AdminStatusBiController::class);
-        Route::resource('type-slik-bi', AdminTypeSlikController::class);
-        Route::resource('promo', AdminPromoController::class);
-        Route::resource('pembayaran', AdminPembayaranController::class);
-        Route::get('data-pembayaran', [AdminDataPembayaranController::class, 'index'])->name('sudah-bayar');
-        Route::get('data-pembayaran-belum', [AdminDataPembayaranController::class, 'belumBayar'])->name('belum-bayar');
-
-        Route::prefix('penjualan')->name('penjualan.')->group(function () {
-            Route::resource('data', AdminPenjualanController::class);
-            Route::resource('hasil', AdminHasilController::class);
-            Route::resource('spk', AdminSPKController::class);
-            Route::get('/penjualan/{id}/payment-data', [AdminPenjualanController::class, 'getPaymentData'])->name('payment-data');
-            Route::get('/penjualan/{id}/print-data', [AdminPenjualanController::class, 'getPrintData'])->name('print-data');
-            Route::get('/penjualan/{id}/getData', [AdminPenjualanController::class, 'getDataPenjualan'])->name('getPenjualan');
-            Route::post('bayar/{id}', [AdminPenjualanController::class, 'bayar'])->name('bayar-dp');
-        });
-        Route::get('cetak-pdf', [AdminSPKController::class, 'cetakPDF'])->name('cetakPDF');
-        Route::post('cicilan-motor/csv/import', [AdminCicilanMotorController::class, 'importCsv'])->name('cicilan.csv.import');
-        Route::post('cicilan-motor/csv/update', [AdminCicilanMotorController::class, 'updateCsv'])->name('cicilan.csv.update');
-        Route::delete('cicilan-motor/cicilan/delete', [AdminCicilanMotorController::class, 'deleteCicilan'])->name('cicilan.delete');
-        // Route::put('cicilan-motor/update-potongan-tenor', [AdminCicilanMotorController::class, 'updatePotonganTenor'])->name('cicilan.potongan-tenor.update');
-
+        
         Route::middleware(['auth.admin:admin'])->group(function () {
+            Route::resource('motor', MotorController::class);
+            Route::resource('type-motor', TypeMotorController::class);
+            Route::resource('merk-motor', MerkMotorController::class);
+            Route::resource('best-motor', BestMotorController::class);
+            Route::resource('leasing-motor', LeasingMotorController::class);
+            Route::resource('kota', KotaController::class);
+            Route::resource('hook', HookController::class);
+            Route::resource('detail-motor', DetailMotorController::class);
+            Route::resource('brosur-motor', BrosurMotorController::class);
+            Route::resource('kota-motor', MotorKotaController::class);
+            Route::resource('blog', BlogController::class);
+            Route::resource('cicilan', AdminCicilanMotorController::class);
+            Route::resource('dealer-motor', AdminDealerController::class);
+            Route::resource('mitra', MitraKamiController::class);
+            Route::resource('diskon-motor', AdminDiskonMotorController::class);
+            Route::resource('mtr-best-motor', AdminMtrBestMotorController::class);
+            Route::resource('event', AdminEventController::class);
+            Route::resource('company-profile', AdminCompanyProfileController::class);
+            Route::resource('slik-bi', AdminSlikController::class);
+            Route::resource('status-bi', AdminStatusBiController::class);
+            Route::resource('type-slik-bi', AdminTypeSlikController::class);
+            Route::resource('promo', AdminPromoController::class);
+            Route::resource('pembayaran', AdminPembayaranController::class);
+            Route::get('data-pembayaran', [AdminDataPembayaranController::class, 'index'])->name('sudah-bayar');
+            Route::get('data-pembayaran-belum', [AdminDataPembayaranController::class, 'belumBayar'])->name('belum-bayar');
+            
+            Route::prefix('penjualan')->name('penjualan.')->group(function () {
+                Route::resource('data', AdminPenjualanController::class);
+                Route::resource('hasil', AdminHasilController::class);
+                Route::resource('spk', AdminSPKController::class);
+                Route::get('/penjualan/{id}/payment-data', [AdminPenjualanController::class, 'getPaymentData'])->name('payment-data');
+                Route::get('/penjualan/{id}/print-data', [AdminPenjualanController::class, 'getPrintData'])->name('print-data');
+                Route::get('/penjualan/{id}/getData', [AdminPenjualanController::class, 'getDataPenjualan'])->name('getPenjualan');
+                Route::post('bayar/{id}', [AdminPenjualanController::class, 'bayar'])->name('bayar-dp');
+            });
+
+            Route::get('cetak-pdf', [AdminSPKController::class, 'cetakPDF'])->name('cetakPDF');
+            Route::post('cicilan-motor/csv/import', [AdminCicilanMotorController::class, 'importCsv'])->name('cicilan.csv.import');
+            Route::post('cicilan-motor/csv/update', [AdminCicilanMotorController::class, 'updateCsv'])->name('cicilan.csv.update');
+            Route::delete('cicilan-motor/cicilan/delete', [AdminCicilanMotorController::class, 'deleteCicilan'])->name('cicilan.delete');
+            // Route::put('cicilan-motor/update-potongan-tenor', [AdminCicilanMotorController::class, 'updatePotonganTenor'])->name('cicilan.potongan-tenor.update');
             Route::resource('/sales', AdminSalesController::class);
             Route::resource('/users', AdminUserController::class);
         });
 
         Route::middleware(['auth.sales:sales'])->group(function () {
+            Route::resource('sales-settings', AdminSalesSettingController::class);
+            Route::prefix('penjualan')->name('penjualan.')->group(function () {
+                Route::get('/penjualan/{id}/payment-data', [AdminPenjualanController::class, 'getPaymentData'])->name('payment-data');
+                Route::get('/penjualan/{id}/print-data', [AdminPenjualanController::class, 'getPrintData'])->name('print-data');
+                Route::get('/penjualan/{id}/getData', [AdminPenjualanController::class, 'getDataPenjualan'])->name('getPenjualan');
+                Route::post('bayar/{id}', [AdminPenjualanController::class, 'bayar'])->name('bayar-dp');
+            });
         });
     });
 });
