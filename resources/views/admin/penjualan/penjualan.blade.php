ee2beed47b663b33cff21c9a8edd005789d747d2
agungsapp <agung.dni19@gmail.com>
2024-03-25 15:44:21 +0700

    Auto filtering data import penjualan, Ceo Hak akses

--- resources/views/admin/penjualan/penjualan.blade.php
+++ resources/views/admin/penjualan/penjualan.blade.php
@@ -4,9 +4,10 @@
 				<div class="row">
 						<div class="col-12">
 								<div class="callout callout-info">
-										<h5><i class="fas fa-info"></i> Note:</h5>
+										<h5><i class="fas fa-info"></i> Catatan:</h5>
 										<p>Lakukan import/update data dengan import file CSV dengan format yang telah ditentukan. Silakan unduh format
-												file CSV <a href="{{ asset('csv/template_penjualan.xlsx') }}" download>di sini</a>.</p>
+												file CSV <a href="{{ asset('csv/template_penjualan.xlsx') }}" download>di sini</a>. <br>
+												Data dengan nik yang sama atau tanggal lahir sama akan otomatis di skip oleh sistem.</p>
 								</div>
 						</div>
 						<div class="col-12 mb-3">
@@ -59,12 +60,17 @@
 												@csrf
 												<div class="card-body">
 														<div class="row">
-																<div class="form-group col-md-6">
+																<div class="form-group col-md-4">
 																		<label for="input-hasil">Nama Konsumen</label>
 																		<input name="konsumen" type="text" class="form-control" placeholder="Masukan nama konsumen"
 																				value="{{ old('konsumen') }}">
 																</div>
-																<div class="form-group col-md-6">
+																<div class="form-group col-md-4">
+																		<label for="input-hasil">NIK Konsumen (Opsional)</label>
+																		<input name="nik" type="text" class="form-control" placeholder="Masukan nik konsumen"
+																				value="{{ old('nik') }}">
+																</div>
+																<div class="form-group col-md-4">
 																		<label>Sales</label>
 																		@if ($sales == null)
 																				<p class="text-danger">Tidak ada data sales silahkan buat terlebih dahulu !</p>
